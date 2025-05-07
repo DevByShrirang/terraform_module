ad6e2a7cd038b1e2a65f5a06ccd37c517b6eecf7
Devops_Learner <shrirang.patil1812@gmail.com>
2025-05-07 11:16:24 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "region_name"  {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "az_zone" {}
+variable "route_table_cidr_block" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "ami_id"  {}
